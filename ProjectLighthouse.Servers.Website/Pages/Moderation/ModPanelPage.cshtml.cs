42fc42f9b7206e721f63cd7ab08a30136d06e3f3
Zhaxxy <67915404+Zhaxxy@users.noreply.github.com>
2025-06-26 20:12:07 +0100

    add palyer list to moderation panel page

--- ProjectLighthouse.Servers.Website/Pages/Moderation/ModPanelPage.cshtml.cs
+++ ProjectLighthouse.Servers.Website/Pages/Moderation/ModPanelPage.cshtml.cs
@@ -4,6 +4,7 @@ using LBPUnion.ProjectLighthouse.Servers.Website.Pages.Layouts;
 using LBPUnion.ProjectLighthouse.Servers.Website.Types;
 using LBPUnion.ProjectLighthouse.Types.Entities.Profile;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LBPUnion.ProjectLighthouse.Servers.Website.Pages.Moderation;
 
@@ -14,12 +15,21 @@ public class ModPanelPage : BaseLayout
 
     public List<AdminPanelStatistic> Statistics = new();
     
+	public List<UserEntity> PlayersOnline = new();
+	
     public async Task<IActionResult> OnGet()
     {
+		
         UserEntity? user = this.Database.UserFromWebRequest(this.Request);
         if (user == null) return this.Redirect("~/login");
         if (!user.IsModerator) return this.NotFound();
-        
+
+
+        List<int> userIds = await this.Database.LastContacts.Where(l => TimeHelper.Timestamp - l.Timestamp < 300)
+            .Select(l => l.UserId)
+            .ToListAsync();
+		this.PlayersOnline = await this.Database.Users.Where(u => userIds.Contains(u.UserId)).ToListAsync();
+		
         this.Statistics.Add(new AdminPanelStatistic(
             statisticNamePlural: "Reports",
             count: await StatisticsHelper.ReportCount(this.Database), 
